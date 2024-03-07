d950768e0b31b08661c6eeaa5dfc8aaca1302fc0
Stoic Coder <shubhmehrotra2022@vitbhopal.ac.in>
2024-03-08 00:29:01 +0530

    navbar changes

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,31 +1,11 @@
 'use client'
 
 import React from 'react'
-import { Menu, X } from 'lucide-react'
 import Image
  from 'next/image'
-const menuItems = [
-  {
-    name: 'Home',
-    href: '#',
-  },
-  {
-    name: 'About',
-    href: '#',
-  },
-  {
-    name: 'Contact',
-    href: '#',
-  },
-]
 
 export default function Navbar() {
-  const [isMenuOpen, setIsMenuOpen] = React.useState(false)
-
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen)
-  }
-
+  
   return (
     <div className="relative w-full bg-[rgba(0, 0, 0, 0.07)] backdrop-blur -mt-[2px]">
       <div className="mx-auto flex w-ful items-center justify-between px-4 py-2 sm:px-6 lg:px-8">
@@ -37,20 +17,7 @@ export default function Navbar() {
             src="/icons/link.png" alt="" />
             <p className='text-lg text-white font-medium'>DevLink</p>
         </div>
-        <div className="hidden lg:block">
-          <ul className="inline-flex justify-evenly space-x-10">
-            {menuItems.map((item) => (
-              <li key={item.name}>
-                <a
-                  href={item.href}
-                  className="text-sm p-2 font-semibold text-slate-300 hover:text-slate-200 hover:border-blue-200 hover:border-b-2 hover:transition-all ease-in-out cursor-pointer"
-                >
-                  {item.name}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </div>
+       
         <div className="hidden lg:block">
           <button
             type="button"
