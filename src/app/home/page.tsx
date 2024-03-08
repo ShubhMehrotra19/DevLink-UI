cb3e782c1e9bd540f02b88fc0a1c6b5db245c796
Stoic Coder <shubhmehrotra2022@vitbhopal.ac.in>
2024-03-08 18:57:52 +0530

    home page grid added

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,14 +1,90 @@
 import React from 'react';
 import Navbar from './Navbar';
+import Cardfront from '@/components/CardComponent/cardFront';
 
 interface Props {}
 
 function Page(props: Props) {
+    const values = [
+        {
+            userName: "Rahul Verma",
+            userDomain: "#webdevelopment", 
+            imageLink: "https://images.pexels.com/photos/1329494/pexels-photo-1329494.jpeg?auto=compress&cs=tinysrgb&w=600", 
+            regNumber: "22BCE10001"
+        },
+        {
+            userName: "Rahul Chautala",
+            userDomain: "#webdevelopment", 
+            imageLink: "https://images.pexels.com/photos/1329494/pexels-photo-1329494.jpeg?auto=compress&cs=tinysrgb&w=600", 
+            regNumber: "22BCE10001"
+        },
+        {
+            userName: "Rahul Chautala",
+            userDomain: "#webdevelopment", 
+            imageLink: "https://images.pexels.com/photos/1329494/pexels-photo-1329494.jpeg?auto=compress&cs=tinysrgb&w=600", 
+            regNumber: "22BCE10001"
+        },
+        {
+            userName: "Rahul Chautala",
+            userDomain: "#webdevelopment", 
+            imageLink: "https://images.pexels.com/photos/1329494/pexels-photo-1329494.jpeg?auto=compress&cs=tinysrgb&w=600", 
+            regNumber: "22BCE10001"
+        },
+        {
+            userName: "Rahul Chautala",
+            userDomain: "#webdevelopment", 
+            imageLink: "https://images.pexels.com/photos/1329494/pexels-photo-1329494.jpeg?auto=compress&cs=tinysrgb&w=600", 
+            regNumber: "22BCE10001"
+        },
+        {
+            userName: "Rahul Chautala",
+            userDomain: "#webdevelopment", 
+            imageLink: "https://images.pexels.com/photos/1329494/pexels-photo-1329494.jpeg?auto=compress&cs=tinysrgb&w=600", 
+            regNumber: "22BCE10001"
+        },
+        {
+            userName: "Rahul Chautala",
+            userDomain: "#webdevelopment", 
+            imageLink: "https://images.pexels.com/photos/1329494/pexels-photo-1329494.jpeg?auto=compress&cs=tinysrgb&w=600", 
+            regNumber: "22BCE10001"
+        },
+        {
+            userName: "Rahul Chautala",
+            userDomain: "#webdevelopment", 
+            imageLink: "https://images.pexels.com/photos/1329494/pexels-photo-1329494.jpeg?auto=compress&cs=tinysrgb&w=600", 
+            regNumber: "22BCE10001"
+        },
+        {
+            userName: "Rahul Chautala",
+            userDomain: "#webdevelopment", 
+            imageLink: "https://images.pexels.com/photos/1329494/pexels-photo-1329494.jpeg?auto=compress&cs=tinysrgb&w=600", 
+            regNumber: "22BCE10001"
+        },
+        {
+            userName: "Rahul Chautala",
+            userDomain: "#webdevelopment", 
+            imageLink: "https://images.pexels.com/photos/1329494/pexels-photo-1329494.jpeg?auto=compress&cs=tinysrgb&w=600", 
+            regNumber: "22BCE10001"
+        },
+        {
+            userName: "Rahul Chautala",
+            userDomain: "#webdevelopment", 
+            imageLink: "https://images.pexels.com/photos/1329494/pexels-photo-1329494.jpeg?auto=compress&cs=tinysrgb&w=600", 
+            regNumber: "22BCE10001"
+        },
+    ]
   const {} = props;
 
   return (
     <section className='bg-white h-screen w-full'>
       <Navbar userImage='https://images.pexels.com/photos/20445473/pexels-photo-20445473.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' />
+      <div className="flex justify-center items-start overflow-y-scroll h-[80%] pt-2">
+  <div className='grid md:grid-cols-2 gap-20'>
+    {values.map((value, index) => (
+      <Cardfront key={index} userName={value.userName} userDomain={value.userDomain} imageLink={value.imageLink} regNumber={value.regNumber} />
+    ))}
+  </div>
+</div>
     </section>
   );
 }
--- src/components/CardComponent/cardFront.tsx
+++ src/components/CardComponent/cardFront.tsx
@@ -5,16 +5,16 @@ interface Props {}
 
 function Cardfront({userName, userDomain, imageLink, regNumber}: any) {
   return (
-    <div className='ml-10 p-6 flex flex-col bg-white w-[600px] h-[350px] rounded flex-grow border-black/[0.4] border-2 -rotate-[12deg] shadow-current shadow-md mb-8 active:scale-95 hover:scale-[102%] transition-all duration-300 cursor-pointer ease-in-out'>
+    <div className='ml-10 p-6 flex flex-col bg-white w-[600px] h-[350px] rounded flex-grow border-black/[0.4] border-2 mb-8 active:scale-95 hover:scale-[102%] transition-all duration-300 cursor-pointer ease-in-out'>
       <div className='flex justify-between items-start'>
         <div className='flex flex-col justify-start items-start'>
-          <Image height={150} width={150} className='mb-3' src={imageLink} alt="" />
+          <Image height={160} width={160} className='mb-3 rounded-full object-cover h-40 w-40' src={imageLink} alt="" />
           <div className="text-black text-4xl font-semibold font-['Poppins']">{userName}</div>
           <div className="text-black text-base font-normal font-['Poppins']">{userDomain}</div>
         </div>
         <Image height={100} width={100} className='-mt-2' src="/card/qrcode.png" alt="" />
       </div>
-      <div className='flex justify-between items-center mt-14'>
+      <div className='flex justify-between items-center mt-12'>
         <div className='flex justify-center items-center gap-6'>
           <Image height={28} width={28} src="/icons/githubcard.png" alt="" />
           <Image height={28} width={28} src="/icons/linkedin.png" alt="" />
