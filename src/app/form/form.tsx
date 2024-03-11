08f6f74d834902bfbcc02db2b0e4d22564b2b6be
Stoic Coder <shubhmehrotra2022@vitbhopal.ac.in>
2024-03-12 01:33:35 +0530

    UI Completed

--- src/app/form/form.tsx
+++ src/app/form/form.tsx
@@ -3,11 +3,6 @@ import React from "react";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { cn } from "../utils/cn";
-import {
-  IconBrandGithub,
-  IconBrandGoogle,
-  IconBrandOnlyfans,
-} from "@tabler/icons-react";
 
 export function SignupFormDemo() {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
