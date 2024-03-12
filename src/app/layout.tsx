7dac163cec957d8d732cf3675b6b1cb1e3c76206
Ishank Lalwani <ishanklalwani@gmail.com>
2024-03-12 23:03:50 +0530

    feat: integrated auth logic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,36 @@
 import type { Metadata } from "next";
 import { Inter, Poppins } from "next/font/google";
+import { getServerSession } from "next-auth";
+import SessionProvider from "../components/Providers/SessionProvider";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
-const poppins = Poppins({ weight: "400" ,subsets: ["latin"] });
+const poppins = Poppins({ weight: "400", subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "DevLink",
   description: "Connect Fast! Connect Better!",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await getServerSession();
+
   return (
-    <html lang="en" style={{
-      scrollbarColor: 'transparent transparent',
-      scrollBehavior: 'smooth',
-      overflowX: 'hidden',
-    }}>
-      <body className={poppins.className}>{children}</body>
+    <html
+      lang="en"
+      style={{
+        scrollbarColor: "transparent transparent",
+        scrollBehavior: "smooth",
+        overflowX: "hidden",
+      }}
+    >
+      <body className={poppins.className}>
+        <SessionProvider session={session}>{children}</SessionProvider>
+      </body>
     </html>
   );
 }
